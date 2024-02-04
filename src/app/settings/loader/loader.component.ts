214f06f2c356a374ba7c3b7bedd2db20b12049eb
krivokhizhin.a <anton.kri97@gmail.com>
2024-02-04 14:52:03 +0300

    get request

--- src/app/settings/loader/loader.component.ts
+++ src/app/settings/loader/loader.component.ts
@@ -1,15 +1,17 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-loader',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.css'
+  styleUrl: './loader.component.css',
 })
 export class LoaderComponent {
   @Output() onLoaderClick: EventEmitter<void> = new EventEmitter();
+  @Input() data: any;
 
   loadHandler() {
     this.onLoaderClick.emit();
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,17 +1,35 @@
-import {ChangeDetectionStrategy, Component, Output} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Output,
+  inject,
+} from '@angular/core';
 import { LoaderComponent } from './loader/loader.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [LoaderComponent],
+  imports: [LoaderComponent, HttpClientModule],
+  // providers: [HttpClientModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsComponent {
+  private http = inject(HttpClient);
+  private cd = inject(ChangeDetectorRef);
+
+  data: any = {};
 
   handlerClick() {
-    console.log('click')
+    this.http
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .subscribe((data) => {
+        this.data = data;
+        this.cd.markForCheck();
+      });
   }
 }
