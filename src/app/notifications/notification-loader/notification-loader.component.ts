788f4f8fdb270329d2f20a30f4fea1ce65828c58
kobyzeva <ankob100@gmail.com>
2024-02-08 13:58:31 +0100

    added some styles for notifications products

--- src/app/notifications/notification-loader/notification-loader.component.ts
+++ src/app/notifications/notification-loader/notification-loader.component.ts
@@ -1,16 +1,33 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-notification-loader',
   templateUrl: './notification-loader.component.html',
-  styleUrl: './notification-loader.component.css'
+  styleUrls: ['./notification-loader.component.css'],
 })
 export class NotificationLoaderComponent {
-  @Output() handlerClick: EventEmitter<void> = new EventEmitter()
+  @Output() handlerClick: EventEmitter<void> = new EventEmitter();
   @Input() products: any;
 
+  currentIndexMap: { [productId: number]: number } = {};
+
+  nextSlide(product: any) {
+    this.currentIndexMap[product.id] = (this.currentIndexMap[product.id] + 1) % product.images.length;
+    console.log('currentIndexMap_next', this.currentIndexMap[product.id] )
+  }
+
+  prevSlide(product: any) {
+    console.log('currentIndexMap_prev', this.currentIndexMap[product.id] )
+    this.currentIndexMap[product.id] = (this.currentIndexMap[product.id] - 1 + product.images.length) % product.images.length;
+  }
+
+  toggleAccordion(product: any) {
+    product.isOpen = !product.isOpen;
+  }
 
   loadHandler() {
     this.handlerClick.emit();
   }
+
+  currentIndex = 0;
 }
--- src/app/notifications/notifications.module.ts
+++ src/app/notifications/notifications.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NotificationsComponent } from './notifications.component';
 import { CommonModule } from '@angular/common';
 import { NotificationLoaderComponent } from './notification-loader/notification-loader.component';
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
 
 @NgModule({
   declarations: [NotificationsComponent, NotificationLoaderComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatIconModule, MatIcon, MatIconButton],
   exports: [NotificationsComponent],
 })
 export class NotificationsModule {}
