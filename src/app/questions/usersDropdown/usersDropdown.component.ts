02398509163b5438df6bedd32c45fdf03c3b470d
Anasta2002 <93197598+Anasta2002@users.noreply.github.com>
2024-02-17 22:30:04 +0100

    rewrote the dropdown.ts

--- src/app/questions/usersDropdown/usersDropdown.component.ts
+++ src/app/questions/usersDropdown/usersDropdown.component.ts
@@ -1,54 +1,95 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import { UsersService } from "../../services/users.service";
-import {RawUser, User} from "../../models";
-import {UserByIdService} from "../../services/user-by-id.service";
-import {FormControl} from "@angular/forms";
-import {debounceTime, map, switchMap} from "rxjs";
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { RawUser, User } from '../../models';
+import { UserByIdService } from '../../services/user-by-id.service';
+import { FormControl } from '@angular/forms';
+import { Subject, map, pairwise, startWith, switchMap, tap } from 'rxjs';
+import { StorageItem } from '../../models/storage-item';
+import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-users-dropdown',
   templateUrl: './usersDropdown.component.html',
   styleUrls: ['./usersDropdown.component.css'],
 })
-export class usersDropdownComponent implements OnInit {
+export class usersDropdownComponent implements OnInit, AfterViewInit {
   initialOptions: any = [];
   result: any;
   selectedUser: any;
   hasFetchedData: boolean = false;
-  myControl = new FormControl('');
-  filteredOptions$ = this.myControl.valueChanges.pipe(
-    debounceTime(300),
-    switchMap((search) => this.usersService.getAllUsers({
-      search: search ?? '',
-      limit: 25,
-    })),
-    map(response => response.users.map(user => new User(user)))
-  )
+  searchControl = new FormControl('');
+  onClick$: Subject<string> = new Subject();
+
+  private savedSearch = new StorageItem<string>('search');
+
+  @ViewChild(MatAutocompleteTrigger, { read: MatAutocompleteTrigger })
+  inputAutoComplete: MatAutocompleteTrigger | undefined;
+
+  filteredOptions$ = this.searchControl.valueChanges.pipe(
+    tap((values) => {
+      console.log(values);
+    }),
+    startWith(''),
+    pairwise(),
+    switchMap(([prevSearch, currentSearch]) =>
+      this.usersService
+        .getUsers({
+          search: currentSearch ?? '',
+          limit: 25,
+          getCached: prevSearch === currentSearch,
+        })
+        .pipe(
+          tap(() => {
+            this.savedSearch.save(currentSearch ?? '');
+          })
+        )
+    ),
+    map((response) => {
+      return response.users.map((user) => new User(user));
+    })
+  );
 
   constructor(
     private usersService: UsersService,
     private userByIdService: UserByIdService,
-    private cdRef: ChangeDetectorRef) {}
+    private cdRef: ChangeDetectorRef
+  ) {}
 
-  ngOnInit(): void {
-    this.myControl.valueChanges.subscribe((value) => {
+  ngOnInit() {}
 
-    })
+  ngAfterViewInit(): void {
+    const search = this.savedSearch.get();
+
+    if (search) {
+    }
+
+    if (this.inputAutoComplete && search) {
+      this.searchControl.setValue(search);
+      this.inputAutoComplete.openPanel();
+    }
+  }
+
+  click() {
+    this.onClick$.next(this.searchControl.value ?? '');
   }
 
   getInitialOptions() {
     if (!this.hasFetchedData) {
-      this.usersService.getAllUsers().subscribe(
-        (data) => {
-          this.initialOptions = data.users.map((user: RawUser) => ({
-            id: user.id,
-            first_name: user.first_name,
-            last_name: user.last_name,
-          }));
-          this.setInitialDataToSessionStorage();
-          this.hasFetchedData = true;
-        }
-      );
+      this.usersService.getUsers().subscribe((response) => {
+        this.initialOptions = response.users.map((user: RawUser) => ({
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+        }));
+        this.setInitialDataToSessionStorage();
+        this.hasFetchedData = true;
+      });
     }
   }
 
@@ -57,18 +98,19 @@ export class usersDropdownComponent implements OnInit {
     if (selectedUser) {
       const selectedUserId = selectedUser.id;
       this.userByIdService.setSelectedUserId(selectedUserId);
-      this.userByIdService.getUserById(selectedUserId).subscribe(
-        (result) => {
-          this.result = result;
-          this.cdRef.detectChanges();
-        },
-      );
+      this.userByIdService.getUserById(selectedUserId).subscribe((result) => {
+        this.result = result;
+        this.cdRef.detectChanges();
+      });
       sessionStorage.setItem('selectedUser', JSON.stringify(selectedUser));
       this.selectedUser = selectedUser;
     }
   }
 
   setInitialDataToSessionStorage() {
-    sessionStorage.setItem('initialOptions', JSON.stringify(this.initialOptions));
+    sessionStorage.setItem(
+      'initialOptions',
+      JSON.stringify(this.initialOptions)
+    );
   }
 }
