904889496df6b73fdb915d2304fa2dfe83a5afda
Anasta2002 <93197598+Anasta2002@users.noreply.github.com>
2024-02-17 22:38:13 +0100

    added modules

--- src/app/questions/questions.module.ts
+++ src/app/questions/questions.module.ts
@@ -7,12 +7,12 @@ import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {MatOption, MatSelect, MatSelectModule} from "@angular/material/select";
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {usersDropdownComponent} from "./usersDropdown/usersDropdown.component";
+import {UsersDropdownComponent} from "./usersDropdown/usersDropdown.component";
 import {UserCardComponent} from "./usersDropdown/user-card/user-card.component";
 import {MatAutocomplete, MatAutocompleteModule} from "@angular/material/autocomplete";
 
 @NgModule({
-  declarations: [QuestionsComponent, usersDropdownComponent],
+  declarations: [QuestionsComponent, UsersDropdownComponent],
   imports: [
     CommonModule,
     MatProgressSpinner,
