5a02ec321d47520e7e3870065bfe145d01abb059
kobyzeva <ankob100@gmail.com>
2024-02-21 07:47:42 +0100

    added an arrow

--- src/app/questions/usersDropdown/users-dropdown.component.ts
+++ src/app/questions/usersDropdown/users-dropdown.component.ts
@@ -1,17 +1,17 @@
-import {Component, inject, ViewChild} from '@angular/core';
-import {UsersService} from '../../services/users.service';
-import {RawUser, User} from '../../models';
-import {FormControl} from '@angular/forms';
-import {debounceTime, map, merge, Subject, switchMap,} from 'rxjs';
-import {StorageItem} from '../../models/storage-item';
-import {MatAutocomplete, MatAutocompleteTrigger,} from '@angular/material/autocomplete';
+import { Component, inject, ViewChild, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { RawUser, User } from '../../models';
+import { FormControl } from '@angular/forms';
+import { debounceTime, map, merge, Subject, switchMap } from 'rxjs';
+import { StorageItem } from '../../models/storage-item';
+import {MatAutocomplete, MatAutocompleteTrigger} from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-users-dropdown',
   templateUrl: './users-dropdown.component.html',
   styleUrls: ['./users-dropdown.component.css'],
 })
-export class UsersDropdownComponent {
+export class UsersDropdownComponent implements OnInit {
   searchControl = new FormControl('');
   onClick$: Subject<string> = new Subject();
 
@@ -20,12 +20,19 @@ export class UsersDropdownComponent {
 
   get user(): User | null {
     const saved = this.savedUser.get();
-
     return saved ? new User(saved) : null;
   }
 
   @ViewChild(MatAutocompleteTrigger, { read: MatAutocompleteTrigger })
   inputAutoComplete: MatAutocompleteTrigger | undefined;
+
+  ngOnInit() {
+    const saved = this.savedUser.get();
+    if (saved) {
+      const fullName = `${saved.first_name} ${saved.last_name}`;
+      this.searchControl.setValue(fullName);
+    }
+  }
 
   users$ = merge(
     this.searchControl.valueChanges.pipe(debounceTime(300)),
@@ -38,9 +45,7 @@ export class UsersDropdownComponent {
       })
     ),
     map((response) => {
-      const users = response.users.map((user) => new User(user));
-
-      return users;
+      return response.users.map((user) => new User(user));
     })
   );
 
@@ -52,18 +57,9 @@ export class UsersDropdownComponent {
     this.onClick$.next(this.searchControl.value ?? '');
   }
 
-  selectUser(
-    user: RawUser,
-    trigger: MatAutocompleteTrigger,
-    auto: MatAutocomplete
-  ) {
-    this.searchControl.setValue('', { emitEvent: false });
+  selectUser(user: RawUser, trigger: MatAutocompleteTrigger, auto: MatAutocomplete) {
+    this.searchControl.setValue( `${user.first_name} ${user.last_name}`);
     this.savedUser.save(user);
-
-    auto.options.forEach((item) => {
-      item.deselect();
-    });
-    this.searchControl.reset('');
   }
 
   deleteCard() {
