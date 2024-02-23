ad8ac491239e140c2744985c993c28eb4b9df6c6
kobyzeva <ankob100@gmail.com>
2024-02-23 10:18:00 +0100

    added arrow

--- src/app/questions/usersDropdown/users-dropdown.component.ts
+++ src/app/questions/usersDropdown/users-dropdown.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { UsersService } from '../../services/users.service';
-import { RawUser, User } from '../../models';
+import { PaginatedUsers, RawUser, User } from '../../models';
 import { FormControl } from '@angular/forms';
-import { debounceTime, map, merge, Subject, switchMap } from 'rxjs';
+import { debounceTime, switchMap, takeUntil } from 'rxjs/operators';
 import { StorageItem } from '../../models/storage-item';
-import {MatAutocomplete, MatAutocompleteTrigger} from '@angular/material/autocomplete';
+import { MatAutocomplete, MatAutocompleteTrigger } from '@angular/material/autocomplete';
+import {of, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-users-dropdown',
@@ -13,10 +14,11 @@ import {MatAutocomplete, MatAutocompleteTrigger} from '@angular/material/autocom
 })
 export class UsersDropdownComponent implements OnInit {
   searchControl = new FormControl('');
-  onClick$: Subject<string> = new Subject();
-
   private savedUser = new StorageItem<RawUser>('selected-user');
-  private usersService = inject(UsersService);
+  private destroy$: Subject<void> = new Subject<void>();
+  private isProgrammaticChange: boolean = false;
+
+  loadedUsers: User[] = [];
 
   get user(): User | null {
     const saved = this.savedUser.get();
@@ -26,43 +28,64 @@ export class UsersDropdownComponent implements OnInit {
   @ViewChild(MatAutocompleteTrigger, { read: MatAutocompleteTrigger })
   inputAutoComplete: MatAutocompleteTrigger | undefined;
 
+  constructor(private usersService: UsersService) {}
+
   ngOnInit() {
     const saved = this.savedUser.get();
     if (saved) {
       const fullName = `${saved.first_name} ${saved.last_name}`;
+      this.isProgrammaticChange = true;
       this.searchControl.setValue(fullName);
+      this.isProgrammaticChange = false;
     }
+
+    this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      switchMap((value) => {
+        if (this.isProgrammaticChange) {
+          return of([]); // Return an empty array for programmatic changes
+        }
+
+        return this.usersService.getUsers({
+          search: value || '',
+          limit: 25,
+        });
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe((response: any) => {
+      if ('users' in response) {
+        this.loadedUsers = response.users.map((user: RawUser) => new User(user));
+      } else {
+        console.error('Unexpected response format:', response);
+      }
+    });
   }
 
-  users$ = merge(
-    this.searchControl.valueChanges.pipe(debounceTime(300)),
-    this.onClick$
-  ).pipe(
-    switchMap((currentSearch) =>
-      this.usersService.getUsers({
-        search: currentSearch ?? '',
-        limit: 25,
-      })
-    ),
-    map((response) => {
-      return response.users.map((user) => new User(user));
-    })
-  );
-
-  constructor() {
-    this.searchControl.valueChanges;
+  onOptionSelected(event: any) {
+    // Prevent triggering a new request when an option is selected
+    this.isProgrammaticChange = true;
+    const selectedUser: RawUser = event.option.value.rawUser;
+    this.searchControl.setValue(`${selectedUser.first_name} ${selectedUser.last_name}`);
+    this.savedUser.save(selectedUser);
+    this.isProgrammaticChange = false;
+    this.inputAutoComplete?.closePanel(); // Close the autocomplete panel after selecting a user
   }
 
   click() {
-    this.onClick$.next(this.searchControl.value ?? '');
-  }
-
-  selectUser(user: RawUser, trigger: MatAutocompleteTrigger, auto: MatAutocomplete) {
-    this.searchControl.setValue( `${user.first_name} ${user.last_name}`);
-    this.savedUser.save(user);
+    // Prevent triggering a new request when the input is clicked
+    this.isProgrammaticChange = true;
+    this.searchControl.setValue(this.searchControl.value || '');
+    this.isProgrammaticChange = false;
   }
 
   deleteCard() {
-    this.savedUser.delete()
+    // Clear loadedUsers array to prevent displaying previous results
+    this.loadedUsers = [];
+    this.savedUser.delete();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
