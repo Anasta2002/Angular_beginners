8be4055c29e8016e0981e362790139d2a6f3b26d
kobyzeva <ankob100@gmail.com>
2024-02-26 08:58:13 +0100

    removed dropdown to avoid errors

--- src/app/questions/questions.module.ts
+++ src/app/questions/questions.module.ts
@@ -8,10 +8,9 @@ import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {MatOption, MatSelect, MatSelectModule} from "@angular/material/select";
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
-import {usersDropdownComponent} from "./usersDropdown/usersDropdown.component";
 
 @NgModule({
-  declarations: [QuestionsComponent, usersDropdownComponent],
+  declarations: [QuestionsComponent],
   imports: [
     CommonModule,
     MatProgressSpinner,
--- src/app/questions/usersDropdown/usersDropdown.component.ts
+++ src/app/questions/usersDropdown/usersDropdown.component.ts
@@ -1,22 +1,22 @@
-import {ChangeDetectorRef, Input, OnChanges, SimpleChanges} from '@angular/core';
-
-@Component({
-  selector: 'app-users-dropdown',
-  templateUrl: './usersDropdown.component.html',
-  styleUrls: ['./usersDropdown.component.css'],
-})
-
-interface User {
-
-}
-export class usersDropdownComponent implements OnChanges  {
-  @Input() users!: User[];
-
-  constructor(private cdr: ChangeDetectorRef) {}
-
-  ngOnChanges(changes: SimpleChanges) {
-    if ('users' in changes) {
-      this.cdr.detectChanges();
-    }
-  }
-}
+// import {ChangeDetectorRef, Input, OnChanges, SimpleChanges} from '@angular/core';
+//
+// @Component({
+//   selector: 'app-users-dropdown',
+//   templateUrl: './usersDropdown.component.html',
+//   styleUrls: ['./usersDropdown.component.css'],
+// })
+//
+// interface User {
+//
+// }
+// export class usersDropdownComponent implements OnChanges  {
+//   @Input() users!: User[];
+//
+//   constructor(private cdr: ChangeDetectorRef) {}
+//
+//   ngOnChanges(changes: SimpleChanges) {
+//     if ('users' in changes) {
+//       this.cdr.detectChanges();
+//     }
+//   }
+// }
